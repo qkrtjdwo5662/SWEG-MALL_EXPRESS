db8fceb8c821448cd59330bf2162428d5f7120d2
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-07-07 05:40:05 +0900

    router test

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const app = express();
 
 app.use(cors());
 
-app.use('/', (req, res) => {
-  res.send("하이");
-})
+//라우터
+const homeRouter = require('./routes/home');
+const testRouter = require('./routes/test');
+
+app.use('/', homeRouter);
+app.use('/test', testRouter);
 
 app.listen(PORT,()=> {
     console.log(`${PORT} 실행 중`);
