1b13ad89bd5d3713b7e5ee7a801de37fa476e13a
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-07-11 15:42:54 +0900

    update userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,27 +7,30 @@ const idDuplicateCheck = async (req, res) => {
         if(!findUser){
             return res.status(200).json('중복 없음 확인');
         }
-        return res.status(409).json('중복 아이디 발견');
+        return res.status(409).json('중복 아이디');
     }catch (err){
         console.log(err);
-        res.status(500).json("회원가입 오류");
+        res.status(500).json("오류 발생");
     }
 }
 
 const signUp = async (req, res) => {
   try{
-     const {user_id, user_pw} = req.body;
-     const findUser = await User.findOne({user_id: req.body.user_id});
-     if(!findUser){
-      await User.create({
-        user_id,
-        user_pw
+     const {user_name, user_gender, user_birth, user_id, user_pw, user_tel, user_email, user_address} = req.body;
+
+     await User.create({
+         user_name,
+         user_gender,
+         user_birth,
+         user_id,
+         user_pw,
+         user_tel,
+         user_email,
+         user_address
       });
+
       console.log(res);
       return res.status(200).json('회원가입 성공');
-     }
-     console.log("중복 아이디 발견");
-     return res.status(409).json("아이디 중복");
   }catch (err){
     console.log(err);
     res.status(500).json('회원기입 오류');
--- models/user.js
+++ models/user.js
@@ -4,15 +4,39 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
-    user_id:{
-      type: String,
-      required: true,
-      unique: true,
-    },
-    user_pw:{
-      type: String,
-      required: true,
-    },
+      user_name:{
+          type:String,
+          required: true,
+      },
+      user_gender:{
+          type: String,
+          required: true,
+      },
+      user_birth : {
+          type: String,
+          required: true,
+      },
+      user_id : {
+          type: String,
+          required: true,
+          unique: true,
+      },
+      user_pw:{
+          type: String,
+          required: true,
+      },
+      user_tel : {
+          type: String,
+          required: true,
+      },
+      user_email : {
+          type: String,
+          required: true,
+      },
+      user_address : {
+          type: String,
+          required: true,
+      }
   },
   {
     collection: 'user',
