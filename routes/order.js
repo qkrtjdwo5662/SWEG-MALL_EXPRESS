460970c2724ddde2ace89a96b8b777479fcc2796
지영 <toto.joung@gmail.com>
2023-08-10 19:27:26 +0900

    orderPage 퍼블

--- routes/order.js
+++ routes/order.js
@@ -8,6 +8,9 @@ const router = express.Router();
 
 router.get('/cart', findProductFromCookieOrUserDB);
 router.get('/addcart/:model', addCart);
+router.get('/order', (req,res)=>{
+  res.render('order', {login:req.session.login})
+})
 router.get('/coupon', (req, res) => {
   res.render('coupon', {login:req.session.login});
 });
